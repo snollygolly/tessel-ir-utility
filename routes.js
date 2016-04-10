a7812ff2bbd7602109e864db2ce667de6a706ea7
snollygolly <snollygolly@gmail.com>
2016-04-10 12:05:34 -0500

    removed all passport stuff

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@
 const config = require("./config.json");
 
 const app = require("./index.js").app;
-const passport = require("./index.js").passport;
 const Router = require("koa-router");
 
 const routes = new Router();
@@ -11,39 +10,9 @@ const routes = new Router();
 const main = require("./controllers/main.js");
 
 // routes
-let user = null;
-
 routes.get("/", function* get() {
-	if (this.isAuthenticated()) {
-		user = this.session.passport.user;
-	}
-	yield this.render("index", {title: config.site.name, user: user});
+	yield this.render("index", {title: config.site.name});
 });
-
-// for passport
-routes.get("/login", function* get() {
-	if (this.isAuthenticated()) {
-		user = this.session.passport.user;
-	}
-	yield this.render("login", {user: user});
-});
-
-routes.get("/logout", function* get() {
-	this.logout();
-	yield this.redirect("/");
-});
-
-// you can add as many strategies as you want
-routes.get("/auth/github",
-	passport.authenticate("github")
-);
-
-routes.get("/auth/github/callback",
-	passport.authenticate("github", {
-		successRedirect: "/account",
-		failureRedirect: "/"
-	})
-);
 
 routes.get("/account", main.account);
 
